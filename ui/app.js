283bf67c59bd8ac7c9f35cfcb89a79e971c66470
Alan Stanley <astanley@upei.ca>
2013-11-26 16:19:44 -0400

    wrapped code

--- ui/app.js
+++ ui/app.js
@@ -1,50 +1,51 @@
-
-Ext.Loader.setConfig({
+$(document).ready(function() {
+  Ext.Loader.setConfig({
     enabled: true
-});
+  });
 
 //var modulePath = '/sites/all/modules/austese_collation';
-var modulePath = Drupal.settings.austese.module_path;
-Ext.Loader.setPath('TableApparatusApp.store',  modulePath + '/ui/app/store');
-Ext.Loader.setPath('TableApparatusApp.model',  modulePath + '/ui/app/model');
-Ext.Loader.setPath('TableApparatusApp.reader',  modulePath + '/ui/app/reader');
-Ext.Loader.setPath('TableApparatusApp.controller', modulePath + '/ui/app/controller');
-Ext.Loader.setPath('TableApparatusApp.view',  modulePath + '/ui/app/view');
-Ext.Loader.setPath('Ext.ux', '/sites/all/libraries/ext-4.1.1a/examples/ux');
+  var modulePath = Drupal.settings.austese.module_path;
+  Ext.Loader.setPath('TableApparatusApp.store', modulePath + '/ui/app/store');
+  Ext.Loader.setPath('TableApparatusApp.model', modulePath + '/ui/app/model');
+  Ext.Loader.setPath('TableApparatusApp.reader', modulePath + '/ui/app/reader');
+  Ext.Loader.setPath('TableApparatusApp.controller', modulePath + '/ui/app/controller');
+  Ext.Loader.setPath('TableApparatusApp.view', modulePath + '/ui/app/view');
+  Ext.Loader.setPath('Ext.ux', '/sites/all/libraries/ext-4.1.1a/examples/ux');
 // keep z-index seed low to avoid interfering with drupal admin overlay
-Ext.WindowMgr.zseed = 1040;
-Ext.application({
+  Ext.WindowMgr.zseed = 1040;
+  Ext.application({
     models: [
-        'VersionListModel',
-        'DocumentListModel'
+      'VersionListModel',
+      'DocumentListModel'
     ],
     stores: [
-        'VersionListStore',
-        'DocumentListStore'
+      'VersionListStore',
+      'DocumentListStore'
     ],
     views: [
-        'ApparatusViewer',
-        'OptionsWindow',
-        'TableView',
-        'VersionView'
+      'ApparatusViewer',
+      'OptionsWindow',
+      'TableView',
+      'VersionView'
     ],
     autoCreateViewport: false,
     name: 'TableApparatusApp',
     controllers: [
-        'TableApparatusAppController'
+      'TableApparatusAppController'
     ],
-    launch: function(){
-        var placeholder = Ext.get('uiplaceholder');
-        var mainWindow = Ext.create('TableApparatusApp.view.ApparatusViewer',{
-            renderTo: Ext.getBody(),
-        }).showAt(placeholder.getX(),placeholder.getY());
-        
-        // render config window
-        Ext.create('TableApparatusApp.view.OptionsWindow').show().hide();
-        
-        var fullscreen = jQuery('#metadata').data('fullscreen');
-        if (fullscreen == 1){
-            this.getController('TableApparatusAppController').toggleFullscreen(Ext.ComponentQuery.query('#toggleFullscreenButton')[0]);
-        }
+    launch: function() {
+      var placeholder = Ext.get('uiplaceholder');
+      var mainWindow = Ext.create('TableApparatusApp.view.ApparatusViewer', {
+        renderTo: Ext.getBody(),
+      }).showAt(placeholder.getX(), placeholder.getY());
+
+      // render config window
+      Ext.create('TableApparatusApp.view.OptionsWindow').show().hide();
+
+      var fullscreen = jQuery('#metadata').data('fullscreen');
+      if (fullscreen == 1) {
+        this.getController('TableApparatusAppController').toggleFullscreen(Ext.ComponentQuery.query('#toggleFullscreenButton')[0]);
+      }
     }
+  });
 });
